a0159133ded50607c28761d32be9a48062dfe471
te9yie <te9yie@users.noreply.github.com>
2021-03-10 11:54:08 +0900

    Use clamp

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,7 @@ fn create_canvas(width: u32, height: u32) -> HtmlCanvasElement {
     canvas
 }
 
-fn request_animation_frame(f: &Closure<dyn FnMut()>) {
+fn request_animation_frame(f: &Closure<dyn FnMut(f64)>) {
     let window = window().unwrap();
     window
         .request_animation_frame(f.as_ref().unchecked_ref())
@@ -35,27 +35,19 @@ pub fn main() {
 
     {
         const DELTA_LIMIT: f64 = 1000.0 / 30.0;
-        let window = window().unwrap();
-        let performance = window.performance().unwrap();
-        let mut last_update_time = performance.now();
-
+        let mut last_update_time = 0.0;
         let f = Rc::new(RefCell::new(None));
         let ff = Rc::clone(&f);
-        *ff.borrow_mut() = Some(Closure::wrap(Box::new(move || {
-            let now = performance.now();
-            let delta = now - last_update_time;
-            let delta = if delta < DELTA_LIMIT {
-                delta
-            } else {
-                DELTA_LIMIT
-            };
+        *ff.borrow_mut() = Some(Closure::wrap(Box::new(move |now| {
+            let delta: f64 /* why need? */ = now - last_update_time;
+            let delta = delta.clamp(0.0, DELTA_LIMIT);
             last_update_time = now;
 
             app.update(delta);
             app.draw();
 
             request_animation_frame(f.borrow().as_ref().unwrap());
-        }) as Box<dyn FnMut()>));
+        }) as Box<dyn FnMut(f64)>));
         request_animation_frame(ff.borrow().as_ref().unwrap());
     }
 }
